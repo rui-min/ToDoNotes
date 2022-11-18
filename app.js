88a3eee559f0ef0624487f98832bda96562dfe0a
rui-min <minruiray@outlook.com>
2022-11-18 10:59:53 -0500

    separation of responsibilities is successful

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const config = require('./utils/config')
-const express = require('express')
+const express = require('express')  // CommonJS module 'import'
 const app = express()
 const cors = require('cors')
 const notesRouter = require('./controllers/notes')
@@ -7,7 +7,7 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
-logger.info('connecting to'. config.MONGODB_URI)
+// logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
